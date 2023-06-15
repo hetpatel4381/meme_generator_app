8f3a4e54606a3832bbd25bdecc48598b1ca10016
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-15 23:50:35 +0530

    here we had done the final touch to save our image of meme

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import Text from '../components/Text';
 import { useSearchParams } from 'react-router-dom';
 import Button from "react-bootstrap/Button";
+import { exportComponentAsJPEG } from 'react-component-export-image';
 
 const EditPage = () => {
 
     const [params] = useSearchParams();
     const [count, setCount] = useState(0);
+
+    const memeRef = createRef();
 
     const addText = () => {
         setCount(count + 1);
@@ -14,7 +17,7 @@ const EditPage = () => {
 
   return (
     <div>
-      <div>
+      <div style={{width:"700px", border:"1px solid"}} ref={memeRef} className='meme mt-5 mb-5'>
         <img src={params.get("url")} width="250px" alt=''/>
         {
             Array(count).fill(0).map((e) => {
@@ -23,6 +26,7 @@ const EditPage = () => {
         }
       </div>
       <Button onClick={addText}>Add Text</Button>
+      <Button variant='success' onClick={(e) => {exportComponentAsJPEG(memeRef)}}>Save</Button>
     </div>
   );
 }
